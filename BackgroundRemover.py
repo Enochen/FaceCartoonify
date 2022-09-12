9af82914a30f407982b7284cbc7e1874c11289dc
Enochen <sirenochen@gmail.com>
2022-09-13 01:10:18 +0200

    add flask app

--- BackgroundRemover.py
+++ BackgroundRemover.py
@@ -6,10 +6,10 @@ class BackgroundRemover:
     def __init__(self):
         self.segmentor = SelfiSegmentation()
 
-    def process(self, img):
+    def process(self, img, threshold=0.50):
         # img = cv2.resize(img, (640, 480))
         color = (255, 255, 255)
-        return self.segmentor.removeBG(img, color, threshold=0.50)
+        return self.segmentor.removeBG(img, color, threshold)
 
 
 
--- index.py
+++ index.py
@@ -1,21 +1,57 @@
 from FaceExtractor import FaceExtractor
 from BackgroundRemover import BackgroundRemover
 from Cartoonifier import Cartoonifier
+from flask import Flask, request, send_file
+from werkzeug.utils import secure_filename
+from io import BytesIO
+from PIL import Image
+import requests
+import io
+import sys
+import numpy as np
+import cv2
 
-def process(img):
+app = Flask(__name__)
+
+def arrToImg(arr):
+    img = Image.fromarray(arr.astype('uint8'))
+    in_memory_file = io.BytesIO()
+    img.save(in_memory_file, "PNG")
+    in_memory_file.seek(0)
+    return in_memory_file
+
+@app.route('/')
+def hello():
+    return 'FaceCartoonify'
+
+@app.route('/cartoonify', methods=['POST'])
+def cartoonify():
     face_extractor = FaceExtractor()
-    bg_remover     = BackgroundRemover()
-    cartoonifier   = Cartoonifier()
+    bg_remover = BackgroundRemover()
+    
+    source = request.files.get('source')
 
-    result_img = img
-    for processor in [face_extractor, cartoonifier, bg_remover]:
-        result_img = processor.process(result_img)
-
-    return result_img
-
-if __name__ == "__main__":
-    import cv2
-    filename = "face1.jpeg"
-    img = cv2.imread(filename)
-
-    cv2.imwrite(f"Extracted_face_{filename}", process(img))
+    in_memory_file = io.BytesIO()
+    source.save(in_memory_file)
+    data = np.fromstring(in_memory_file.getvalue(), np.uint8)
+    decoded = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
+    decoded = bg_remover.process(decoded)
+    decoded = face_extractor.process(decoded)
+    
+    files = {
+        'file_type': (None, 'image'),
+        'source': (secure_filename(source.filename), arrToImg(decoded), source.content_type)
+    }
+    res = requests.post(
+        'https://master-white-box-cartoonization-psi1104.endpoint.ainize.ai/predict', files=files)
+    
+    data = np.fromstring(res.content, np.uint8)
+    decoded = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
+    decoded = bg_remover.process(decoded, 0.3)
+    ad = np.concatenate([decoded, np.full((decoded.shape[0], decoded.shape[1], 1), 255, dtype=np.uint8)], axis=-1)
+    white = np.all(decoded == [255,255,255], axis=-1)
+    print(decoded, file=sys.stderr)
+    print(white, file=sys.stderr)
+    ad[white, -1] = 0
+    final_img = arrToImg(ad)
+    return send_file(final_img, mimetype=source.content_type)
